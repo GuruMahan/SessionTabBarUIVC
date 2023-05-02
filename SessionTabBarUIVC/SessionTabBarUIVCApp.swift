e146e76ef93d223b0211bf30a4ff8f8959a187eb
Guru Mahan <gurumahan@digivalsolutions.com>
2023-05-02 16:49:41 +0530

    Initial Commit

---
+++ SessionTabBarUIVC/SessionTabBarUIVCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SessionTabBarUIVCApp.swift
+//  SessionTabBarUIVC
+//
+//  Created by Guru Mahan on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct SessionTabBarUIVCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
